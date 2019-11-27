46e6963c069335178459b43d8d21c63c1e6f027f
Igor Derkach <gosha753951@gmail.com>
2019-11-27 10:49:26 +0300

    Make migrations graph

---
+++ mongoengine_migrate/migration.py
@@ -0,0 +1,220 @@
+from mongoengine_migrate.exceptions import MigrationError
+from mongoengine_migrate.utils import Slotinit
+
+
+class Migration(Slotinit):
+    """Object represents one migration
+
+    Contains information which is set in migration:
+    * name -- migration file name without '.py' suffix
+    * dependencies -- name list of migrations which this migration is
+      dependent by
+    * applied -- is migration was applied or not. Taken from database
+    """
+    __slots__ = ('name', 'dependencies', 'applied')
+    defaults = {'applied': False}
+
+
+class MigrationsGraph:
+    def __init__(self):
+        # Following two variables contains the same migrations digraph
+        # but from different points of view
+        self._parents = {}  # {child_name: [parent_obj...]}
+        self._children = {}  # {parent_name: [child_obj...]}
+
+        self._migrations = {}  # {migration_name: migration_obj}
+
+    @property
+    def initial(self):
+        """Return initial migration object"""
+        for name, parents in self._parents.items():
+            if not parents:
+                return self._migrations[name]
+
+    @property
+    def last(self):
+        """Return last children migration object"""
+        for name, children in self._children.items():
+            if not children:
+                return self._migrations[name]
+
+    @property
+    def migrations(self):
+        """Migration objects dict"""
+        return self._migrations
+
+    def add(self, migration: Migration):
+        """
+        Add migration to the graph. If object with that name exists
+        in graph then it will be replaced.
+        :param migration: Migration object
+        :return:
+        """
+        self._parents[migration.name] = []
+        self._children[migration.name] = []
+
+        for partner in self._migrations:
+            if partner.name == migration.name:
+                continue
+            if partner.name in migration.dependencies:
+                self._parents[migration.name].append(partner)
+                self._children[partner.name].append(migration)
+            if migration.name in partner.dependencies:
+                self._children[migration.name].append(partner)
+                self._parents[partner.name].append(migration)
+
+        self._migrations[migration.name] = migration
+
+    def verify(self):
+        """
+        Verify migrations graph to be satisfied to consistency rules
+        Graph must not have loops, disconnections.
+        Also it should have single initial migration and (for a while)
+        single last migration.
+        :raises MigrationError: if problem in graph was found
+        :return:
+        """
+        initials = []
+        last_children = []
+
+        for name, obj in self._migrations.items():
+            if not self._parents[name]:
+                initials.append(name)
+            if not self._children[name]:
+                last_children.append(name)
+            if len(obj.dependencies) > len(self._parents[name]):
+                diff = set(obj.dependencies) - {x.name for x in self._parents[name]}
+                raise MigrationError(f'Unknown dependencies in migration {name!r}: {diff}')
+            if name in (x.name for x in self._children[name]):
+                raise MigrationError(f'Found migration which dependent on itself: {name!r}')
+
+        if len(initials) == len(last_children) and len(initials) > 1:
+            raise MigrationError(f'Migrations graph is disconnected, history segments '
+                                 f'started on: {initials!r}, ended on: {last_children!r}')
+        if len(initials) > 1:
+            raise MigrationError(f'Several initial migrations found: {initials!r}')
+
+        if len(last_children) > 1:
+            raise MigrationError(f'Several last migrations found: {last_children!r}')
+
+        if not initials or not last_children:
+            raise MigrationError(f'No initial or last children found')
+
+    def walk_down(self, from_node: Migration, unapplied_only=True, _node_counters=None):
+        """
+        Walks down over migrations graph. Iterates in order as migrations
+        should be applied.
+
+        We're used modified DFS (depth-first search) algorithm to traverse
+        the graph. Migrations are built into directed graph (digraph)
+        counted from one root node to the last ones. Commonly DFS tries to
+        walk to the maximum depth firstly.
+
+        But there are some problems:
+        * Graph can have directed cycles. This happens when some
+          migration has several dependencies. Therefore we'll walk
+          over such migration several times
+        * Another problem arises from the first one. Typically we must walk
+          over all dependencies before a dependent migration will be
+          touched. DFS will process only one dependency before get to
+          a dependent migration
+
+        In order to manage it we use counter for each migration
+        (node in digraph) initially equal to its parents count.
+        Every time the algorithm gets to node it decrements this counter.
+        If counter > 0 after that then don't touch this node and
+        break traversing on this depth and go up. If counter == 0 then
+        continue traversing.
+        :param from_node: current node in graph
+        :param unapplied_only: if True then return only unapplied migrations
+         or return all migrations otherwise
+        :param _node_counters:
+        :raises MigrationError: if graph has a closed cycle
+        :return: Migration objects generator
+        """
+        # FIXME: may yield nodes not related to target migration if branchy graph
+        # FIXME: if migration was applied after its dependencies unapplied then it is an error
+        if _node_counters is None:
+            _node_counters = {}
+        if from_node is None:
+            return ()
+        _node_counters.setdefault(from_node.name, len(self._parents[from_node.name]))
+        _node_counters[from_node.name] -= 1
+
+        if _node_counters[from_node.name] > 0:
+            # Stop on this depth if not all parents has been viewed
+            return
+
+        if _node_counters[from_node.name] < 0:
+            # A node was already returned and we're reached it again
+            # This means there is a closed cycle
+            raise MigrationError(f'Found closed cycle in migration graph, '
+                                 f'{from_node.name!r} is repeated twice')
+
+        if not (from_node.applied and unapplied_only):
+            yield from_node
+
+        for child in self._children[from_node.name]:
+            yield from self.walk_down(child, unapplied_only, _node_counters)
+
+    def walk_up(self, from_node: Migration, applied_only=True, _node_counters=None):
+        """
+        Walks up over migrations graph. Iterates in order as migrations
+        should be reverted.
+
+        We're using modified DFS (depth-first search) algorithm which in
+        reversed order (see `walk_down`). Instead of looking at node
+        parents count we're consider children count in order to return
+        all dependent nodes before dependency.
+
+        Because of the migrations graph may have many orphan child nodes
+        they all should be passed as parameter
+        :param from_node:  last children node we are starting for
+        :param applied_only: if True then return only applied migrations,
+         return all migrations otherwise
+        :param _node_counters:
+        :raises MigrationError: if graph has a closed cycle
+        :return: Migration objects generator
+        """
+        # FIXME: may yield nodes not related to reverting if branchy graph
+        # FIXME: if migration was unapplied before its dependencies applied then it is an error
+        if _node_counters is None:
+            _node_counters = {}
+        if from_node is None:
+            return ()
+        _node_counters.setdefault(from_node.name, len(self._parents[from_node.name]))
+        _node_counters[from_node.name] -= 1
+
+        if _node_counters[from_node.name] > 0:
+            # Stop in this depth if not all children has been viewed
+            return
+
+        if _node_counters[from_node.name] < 0:
+            # A node was already returned and we're reached it again
+            # This means there is a closed cycle
+            raise MigrationError(f'Found closed cycle in migration graph, '
+                                 f'{from_node.name!r} is repeated twice')
+
+        if from_node.applied or not applied_only:
+            yield from_node
+
+        for child in self._parents[from_node.name]:
+            yield from self.walk_up(child, applied_only, _node_counters)
+
+    def __iter__(self):
+        return iter(self.walk_down(self.initial, unapplied_only=False))
+
+    def __reversed__(self):
+        return iter(self.walk_up(self.last, applied_only=False))
+
+    def __contains__(self, migration):
+        return migration in self._migrations.values()
+
+    def __eq__(self, other):
+        if other is self:
+            return True
+
+        return all(migr == other._migrations.get(name) for name, migr in self._migrations.items())
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
---
+++ tests/test_migration.py
@@ -0,0 +1,469 @@
+import random
+
+import pytest
+
+from mongoengine_migrate.migration import Migration, MigrationsGraph
+from mongoengine_migrate.exceptions import MigrationError
+
+
+@pytest.fixture
+def migration_tree():
+    """
+      _____(01)_____
+     V      V       V
+    (02)   (03)    (04)__
+     |      |      V     V
+      \     /    (05)  (06)
+       \   /_____/  \  /
+        V VV         VV
+        (07)        (08)
+           \___  ___/
+               VV
+              (09)
+               V
+              (10)
+    """
+    return [
+        Migration(name='01', dependencies=[]),
+        Migration(name='02', dependencies=['01']),
+        Migration(name='03', dependencies=['01']),
+        Migration(name='04', dependencies=['01']),
+        Migration(name='05', dependencies=['04']),
+        Migration(name='06', dependencies=['04']),
+        Migration(name='07', dependencies=['02', '03', '05']),
+        Migration(name='08', dependencies=['05', '06']),
+        Migration(name='09', dependencies=['07', '08']),
+        Migration(name='10', dependencies=['09']),
+    ]
+
+
+class TestMigrationGraph:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.obj = MigrationsGraph()
+
+    def test_initial_prop__should_return_initial_migration_object(self, migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+
+        res = self.obj.initial
+
+        assert res == migration_tree[0]
+
+    def test_initial_prop__on_empty_graph__should_return_none(self):
+        res = self.obj.initial
+
+        assert res is None
+
+    def test_last_prop__should_return_last_migration(self, migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+
+        res = self.obj.last
+
+        assert res == migration_tree[-1]
+
+    def test_last_prop__on_empty_graph__should_return_none(self):
+        res = self.obj.last
+
+        assert res is None
+
+    def test_add__on_migration_order_is_shuffled__should_fill_parents_correctly(self,
+                                                                                migration_tree):
+        random.shuffle(migration_tree)
+        migrations = {m.name: m for m in migration_tree}
+        expect_parents = {
+            '01': [],
+            '02': [migrations['01']],
+            '03': [migrations['01']],
+            '04': [migrations['01']],
+            '05': [migrations['04']],
+            '06': [migrations['04']],
+            '07': [migrations['02'], migrations['03'], migrations['05']],
+            '08': [migrations['05'], migrations['06']],
+            '09': [migrations['07'], migrations['08']],
+            '10': [migrations['09']]
+        }
+
+        for m in migration_tree:
+            self.obj.add(m)
+
+        assert self.obj._parents.keys() == expect_parents.keys()
+        assert all(e in self.obj._parents[k]
+                   for k in expect_parents.keys()
+                   for e in expect_parents[k])
+
+    def test_add__on_migration_order_is_shuffled__should_fill_children_correctly(self,
+                                                                                 migration_tree):
+        random.shuffle(migration_tree)
+        migrations = {m.name: m for m in migration_tree}
+        expect_children = {
+            '01': [migrations['02'], migrations['03'], migrations['04']],
+            '02': [migrations['07']],
+            '03': [migrations['07']],
+            '04': [migrations['05'], migrations['06']],
+            '05': [migrations['07'], migrations['08']],
+            '06': [migrations['08']],
+            '07': [migrations['09']],
+            '08': [migrations['09']],
+            '09': [migrations['10']],
+            '10': []
+        }
+
+        for m in migration_tree:
+            self.obj.add(m)
+
+        assert self.obj._children.keys() == expect_children.keys()
+        assert all(e in self.obj._children[k]
+                   for k in expect_children.keys()
+                   for e in expect_children[k])
+
+    def test_add__on_migration_order_is_shuffled__should_fill_migrations_correctly(self,
+                                                                                   migration_tree):
+        random.shuffle(migration_tree)
+        expect_migrations = {m.name: m for m in migration_tree}
+
+        for m in migration_tree:
+            self.obj.add(m)
+
+        assert self.obj.migrations.items() == expect_migrations.items()
+
+    def test_add__if_migration_already_in_graph__should_replace_migration_object(self,
+                                                                                 migration_tree):
+        random.shuffle(migration_tree)
+        another_migration = Migration(name='05', dependencies=['04', '03'], applied=True)
+
+        for m in migration_tree:
+            self.obj.add(m)
+        self.obj.add(another_migration)
+
+        assert self.obj.migrations['05'] is another_migration
+
+    def test_add__if_migration_already_in_graph__should_rebuild_parents_links(self, migration_tree):
+        random.shuffle(migration_tree)
+        migrations = {m.name: m for m in migration_tree}
+        expect_parents = {
+            '01': [],
+            '02': [migrations['01']],
+            '03': [migrations['01']],
+            '04': [migrations['01']],
+            '05': [migrations['04'], migrations['03']],
+            '06': [migrations['04']],
+            '07': [migrations['02'], migrations['03'], migrations['05']],
+            '08': [migrations['05'], migrations['06']],
+            '09': [migrations['07'], migrations['08']],
+            '10': [migrations['09']]
+        }
+        another_migration = Migration(name='05', dependencies=['04', '03'], applied=True)
+
+        for m in migration_tree:
+            self.obj.add(m)
+        self.obj.add(another_migration)
+
+        assert self.obj._parents.keys() == expect_parents.keys()
+        assert all(e in self.obj._parents[k]
+                   for k in expect_parents.keys()
+                   for e in expect_parents[k])
+
+    def test_add__if_migration_already_in_graph__should_rebuild_children_links(self, migration_tree):
+        random.shuffle(migration_tree)
+        migrations = {m.name: m for m in migration_tree}
+        expect_children = {
+            '01': [migrations['02'], migrations['03'], migrations['04']],
+            '02': [migrations['07']],
+            '03': [migrations['07']],
+            '04': [migrations['05'], migrations['06']],
+            '05': [migrations['07'], migrations['08']],
+            '06': [migrations['08']],
+            '07': [migrations['09']],
+            '08': [migrations['09']],
+            '09': [migrations['10']],
+            '10': []
+        }
+        another_migration = Migration(name='05', dependencies=['04', '03'], applied=True)
+
+        for m in migration_tree:
+            self.obj.add(m)
+        self.obj.add(another_migration)
+
+        assert self.obj._children.keys() == expect_children.keys()
+        assert all(e in self.obj._children[k]
+                   for k in expect_children.keys()
+                   for e in expect_children[k])
+
+    def test_verify__if_graph_is_correct__should_return_nothing(self, migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+
+        res = self.obj.verify()
+
+        assert res is None
+
+    def test_verify__if_graph_is_empty__should_return_nothing(self):
+        res = self.obj.verify()
+
+        assert res is None
+
+    def test_verify__if_wrong_dependency__should_raise_migration_error(self, migration_tree):
+        migration_tree[4] = Migration(name='05', dependencies=['wrong_dep'])
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'Unknown dependencies' in str(e)
+        assert '05' in str(e)
+
+    def test_verify__if_migration_is_dependent_on_itself__should_raise_migration_error(self,
+                                                                                       migration_tree):
+        migration_tree[4] = Migration(name='05', dependencies=['05'])
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'dependent on itself' in str(e)
+        assert '05' in str(e)
+
+    def test_verify__if_graph_has_disconnects__should_raise_migration_error(self, migration_tree):
+        migration_tree[8] = Migration(name='09', dependencies=[])
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'Migrations graph is disconnected' in str(e)
+        assert '09' in str(e)
+
+    def test_verify__if_graph_has_several_initial_migrations__should_raise_migration_error(
+            self,
+            migration_tree
+    ):
+        migration_tree[4] = Migration(name='05', dependencies=[])
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'Several initial migrations' in str(e)
+        assert '05' in str(e)
+
+    def test_verify__if_graph_has_several_last_migrations__should_raise_migration_error(
+            self,
+            migration_tree
+    ):
+        migration_tree.append(Migration(name='11', dependencies=['09']))
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'Several last migrations' in str(e)
+        assert '11' in str(e)
+
+    def test_verify__if_graph_has_no_initial_or_last_migration__should_raise_migration_error(
+            self,
+            migration_tree
+    ):
+        migration_tree[0] = Migration(name='01', dependencies=['10'])
+        for m in migration_tree:
+            self.obj.add(m)
+
+        with pytest.raises(MigrationError) as e:
+            self.obj.verify()
+
+        assert 'No initial or last children' in str(e)
+
+    @pytest.mark.parametrize('from_node,expect_order', (
+        ('01', ['01', '02', '03', '04', '05', '07', '06', '08', '09', '10']),
+        ('05', ['05']),
+        ('04', ['04', '05', '06']),
+        ('09', ['09', '10']),
+        ('10', ['10'])
+    ))
+    def test_walk_down__should_walk_from_start_node_considering_dependencies(
+            self,
+            migration_tree,
+            from_migration,
+            expect_order
+    ):
+        for m in migration_tree:
+            self.obj.add(m)
+        migrations = {m.name: m for m in migration_tree}
+
+        res = self.obj.walk_down(migrations[from_migration])
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_down__if_some_migrations_were_already_applied__should_not_return_them_by_default(
+            self,
+            migration_tree
+    ):
+        migrations = {m.name: m for m in migration_tree}
+        for i in ('01', '02', '03', '04', '08'):
+            migrations[i].applied = True
+        for m in migrations.values():
+            self.obj.add(m)
+        expect_order = ['05', '07', '06', '09', '10']
+
+        res = self.obj.walk_down(self.obj.initial)
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_down__if_some_migrations_were_applied_and_forced_to_return__should_return_all(
+            self,
+            migration_tree
+    ):
+        migrations = {m.name: m for m in migration_tree}
+        for i in ('01', '02', '03', '04', '08'):
+            migrations[i].applied = True
+        for m in migrations.values():
+            self.obj.add(m)
+        expect_order = ['01', '02', '03', '04', '05', '07', '06', '08', '09', '10']
+
+        res = self.obj.walk_down(self.obj.initial, unapplied_only=False)
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_down__if_no_migrations_added__should_return_empty_generator(self):
+        res = self.obj.walk_down(self.obj.initial)
+
+        assert list(res) == []
+
+    @pytest.mark.parametrize('from_node,expect_order', (
+        ('10', ['10', '09', '07', '02', '03', '08', '05', '06', '04', '01']),
+        ('08', ['08', '06']),
+        ('07', ['07', '02', '03']),
+        ('03', ['03']),
+        ('01', ['01'])
+    ))
+    def test_walk_up__should_walk_from_start_node_considering_dependencies(
+            self,
+            migration_tree,
+            from_migration,
+            expect_order
+    ):
+        for m in migration_tree:
+            self.obj.add(m)
+        migrations = {m.name: m for m in migration_tree}
+
+        res = self.obj.walk_up(migrations[from_migration])
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_up__if_some_migrations_were_already_unapplied__should_not_return_them_by_default(
+            self,
+            migration_tree
+    ):
+        migrations = {m.name: m for m in migration_tree}
+        for i in ('01', '02', '03', '04', '09'):
+            migrations[i].applied = True
+        for m in migrations.values():
+            self.obj.add(m)
+        expect_order = ['10', '07', '02', '03', '08', '05', '06']
+
+        res = self.obj.walk_up(self.obj.last)
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_up__if_some_migrations_were_unapplied_and_forced_to_return__should_return_all(
+            self,
+            migration_tree
+    ):
+        migrations = {m.name: m for m in migration_tree}
+        for i in ('01', '02', '03', '04', '09'):
+            migrations[i].applied = True
+        for m in migrations.values():
+            self.obj.add(m)
+        expect_order = ['10', '09', '07', '02', '03', '08', '05', '06', '04', '01']
+
+        res = self.obj.walk_up(self.obj.last, applied_only=False)
+
+        assert [m.name for m in res] == expect_order
+
+    def test_walk_up__if_no_migrations_added__should_return_empty_generator(self):
+        res = self.obj.walk_up(self.obj.last)
+
+        assert list(res) == []
+
+    def test_iter__should_walk_down_from_the_initial_node(self, migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+        expect_order = ['01', '02', '03', '04', '05', '07', '06', '08', '09', '10']
+
+        assert list(x.name for x in self.obj) == expect_order
+
+    def test_reversed__should_walk_up_from_the_last_node(self, migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+        expect_order = ['10', '09', '07', '02', '03', '08', '05', '06', '04', '01']
+
+        assert list(x.name for x in reversed(self.obj)) == expect_order
+
+    def test_contains__if_migration_is_contained_in_graph__should_return_true(self,
+                                                                              migration_tree):
+        for m in migration_tree:
+            self.obj.add(m)
+
+        assert migration_tree[3] in self.obj
+
+    def test_contains__if_migration_is_not_contained_in_graph__should_return_false(self,
+                                                                                   migration_tree):
+        for m in migration_tree[:3]:
+            self.obj.add(m)
+
+        assert migration_tree[7] not in self.obj
+
+    def test_eq_ne__migrations_are_equal__graphs_should_be_equal(self, migration_tree):
+        obj = MigrationsGraph()
+        for m in migration_tree:
+            self.obj.add(m)
+            obj.add(m)
+
+        assert self.obj == obj
+        assert not self.obj != obj
+
+    def test_eq_ne__if_two_migrations_in_graphs_are_differ__graphs_should_be_not_equal(
+            self,
+            migration_tree
+    ):
+        obj = MigrationsGraph()
+        test_migration05 = Migration(name="test_name", dependencies=['04'])
+        for m in migration_tree:
+            self.obj.add(m)
+            obj.add(m)
+        obj.migrations['05'] = test_migration05
+
+        assert not self.obj == obj
+        assert not obj == self.obj
+        assert self.obj != obj
+        assert obj != self.obj
+
+    def test_eq_ne__if_extra_migration_in_second_graph__graphs_should_be_not_equal(self,
+                                                                                   migration_tree):
+        obj = MigrationsGraph()
+        test_migration05 = Migration(name="test_name", dependencies=['04'])
+        for m in migration_tree:
+            self.obj.add(m)
+            obj.add(m)
+        obj.add(test_migration05)
+
+        assert not self.obj == obj
+        assert not obj == self.obj
+        assert self.obj != obj
+        assert obj != self.obj
+
+    def test_eq_ne__if_second_graph_is_empty__graphs_should_be_not_equal(self, migration_tree):
+        obj = MigrationsGraph()
+        for m in migration_tree:
+            self.obj.add(m)
+
+        assert not self.obj == obj
+        assert not obj == self.obj
+        assert self.obj != obj
+        assert obj != self.obj

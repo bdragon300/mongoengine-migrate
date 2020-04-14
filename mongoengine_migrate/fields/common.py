4cc441eba51427b5b42389cdbd34c81dd231a3ea
Igor Derkach <gosha753951@gmail.com>
2020-04-14 16:01:06 +0300

    Add some field types

--- mongoengine_migrate/fields/common.py
+++ mongoengine_migrate/fields/common.py
@@ -1,16 +1,14 @@
-from mongoengine import (
-    StringField,
-    IntField,
-)
+import mongoengine.fields
 
-from .base import CommonFieldType, AlterDiff
+from .base import CommonFieldType
+from ..actions.diff import AlterDiff
 from mongoengine_migrate.exceptions import MigrationError
 from typing import List, Dict, Any, Type
 import mongoengine.fields
 import re
 
 
-class NumberFieldType(CommonFieldType):
+class NumberFieldTypeBase(CommonFieldType):
     """Base class for number field types"""
     @classmethod
     def schema_skel(cls) -> dict:
@@ -20,12 +18,13 @@ class NumberFieldType(CommonFieldType):
         return skel
 
     def change_min_value(self, diff: AlterDiff):
+        # FIXME: treat None in diff old/new as value absence. Here and further
         if diff.policy == 'modify':
             self.collection.update_many(
                 {self.db_field: {'$lt': diff.new}},
                 {'$set': {self.db_field: diff.new}}
             )
-
+        # FIXME: make replacing on error only here and further
         if diff.policy == 'replace':
             if diff.default < diff.new:
                 raise MigrationError(f'Cannot set min_value for '
@@ -55,7 +54,11 @@ class NumberFieldType(CommonFieldType):
 
 
 class StringFieldType(CommonFieldType):
-    mongoengine_field_classes = [StringField]
+    mongoengine_field_classes = [
+        mongoengine.fields.StringField,
+        mongoengine.fields.URLField,   # TODO: implement checks
+        mongoengine.fields.EmailField  # TODO: implement checks
+    ]
 
     @classmethod
     def schema_skel(cls):
@@ -138,11 +141,104 @@ class StringFieldType(CommonFieldType):
         self._convertion_command('$toString')
 
 
-class IntFieldType(NumberFieldType):
-    mongoengine_field_classes = [IntField]
+class IntFieldType(NumberFieldTypeBase):
+    mongoengine_field_classes = [mongoengine.fields.IntField]
 
     def convert_from(self,
                      from_field_cls: Type[mongoengine.fields.BaseField],
                      to_field_cls: Type[mongoengine.fields.BaseField]):
         self._convertion_command('$toInt')
 
+
+class LongFieldType(NumberFieldTypeBase):
+    mongoengine_field_classes = [mongoengine.fields.LongField]
+
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        self._convertion_command('$toLong')
+
+
+class FloatFieldType(NumberFieldTypeBase):
+    mongoengine_field_classes = [mongoengine.fields.FloatField]
+
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        self._convertion_command('$toDouble')
+
+
+class DecimalFieldType(NumberFieldTypeBase):
+    mongoengine_field_classes = [mongoengine.fields.DecimalField]
+
+    def change_force_string(self, diff: AlterDiff):
+        if diff.policy == 'modify':
+            if diff.new is True:
+                self._convertion_command('$toString')
+            else:
+                self._convertion_command('$toDecimal')
+
+        # TODO: implement 'replace'
+
+    def change_precision(self, diff: AlterDiff):
+        """This one is related only for python. Nothing to do"""
+        pass
+
+    def change_rounding(self, diff: AlterDiff):
+        """This one is related only for python. Nothing to do"""
+        pass
+
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        if self.field_schema.get('force_string', False):
+            self._convertion_command('$toString')
+        else:
+            self._convertion_command('$toDecimal')
+
+
+class DateTimeFieldType(CommonFieldType):
+    mongoengine_field_classes = [
+        mongoengine.fields.DateTimeField,
+        mongoengine.fields.DateField
+    ]
+
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        self._convertion_command('$toDate')
+
+
+class ComplexDateTimeFieldType(StringFieldType):
+    mongoengine_field_classes = [mongoengine.fields.ComplexDateTimeField]
+
+    def change_separator(self, diff: AlterDiff):
+        old_sep = re.escape(diff.old)
+        old_format = ''.join(('^', old_sep.join(["\\d{4}"] + ["\\d{2}"] * 5 + ["\\d{6}"]), '$'))
+
+        if diff.policy == 'modify':
+            replace_expr = [(f'field[{c}]', diff.new) for c in range(7)]
+            replace_expr = [x for pair in replace_expr for x in pair][:-1]
+            self.collection.aggregate([
+                {'$match': {self.db_field: {'$regex': old_format}}},
+                {'$addFields': {self.db_field: {'$split': ['$field', diff.old]}}},
+                {'$addFields': {self.db_field: {'$concat': replace_expr}}},
+                {'$out': self.collection.name}
+            ])
+
+        if diff.policy == 'replace':
+            self.collection.update_many({self.db_field: {'$not': {'$regex': old_format}}},
+                                        {'$set': {self.db_field: diff.default}})
+
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        pass  #TODO
+
+
+class EmbeddedDocumentFieldType(CommonFieldType):
+    # TODO: implement embedded documents
+    def convert_from(self,
+                     from_field_cls: Type[mongoengine.fields.BaseField],
+                     to_field_cls: Type[mongoengine.fields.BaseField]):
+        pass

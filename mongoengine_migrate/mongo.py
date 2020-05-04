e0f9c67dd2f20b672cc591ae4b2c2a1deee8d0e3
Igor Derkach <gosha753951@gmail.com>
2020-05-04 20:00:28 +0300

    Implement mongodb version restriction decorator

--- mongoengine_migrate/mongo.py
+++ mongoengine_migrate/mongo.py
@@ -1,6 +1,9 @@
+import functools
+
 from pymongo.collection import Collection
 
 from mongoengine_migrate.exceptions import MigrationError
+from . import flags
 
 
 def check_empty_result(collection: Collection, db_field: str, find_filter: dict):
@@ -21,3 +24,38 @@ def check_empty_result(collection: Collection, db_field: str, find_filter: dict)
         raise MigrationError(f"Field {collection.name}.{db_field} in some records "
                              f"has wrong values. First several examples: "
                              f"{','.join(examples)}")
+
+
+def mongo_version(min_version=None, max_version=None, throw_error=False):
+    """
+    Restrict the decorated function execution by MongoDB version.
+
+    If current db version is out of specified range then the function
+    either won't get executed or error will be raised, depending on
+    `throw_error` parameter
+    :param min_version: Minimum MongoDB version (including)
+    :param max_version: Maximum MongoDB version (excluding)
+    :param throw_error: If False then function call will just silently
+     skipped on version mismatch. If True then `MigrationError`
+     exception will be raised then.
+    :return:
+    """
+    assert min_version or max_version
+
+    def dec(f):
+        @functools.wraps(f)
+        def w(*args, **kwargs):
+            invalid = min_version and flags.mongo_version < min_version \
+                      or max_version and flags.mongo_version >= max_version
+
+            if invalid and throw_error:
+                version_msg = ', '.join([
+                    (">=" + min_version if min_version else ""),
+                    ("<" + max_version if max_version else "")
+                ])
+                raise MigrationError(f'Commands are valid only for MongoDB version {version_msg}')
+            elif not invalid:
+                return f(*args, **kwargs)
+
+        return w
+    return dec

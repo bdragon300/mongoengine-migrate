58ecb81e7c7af97e29250cbf09f264f2db79fff1
Igor Derkach <gosha753951@gmail.com>
2020-05-19 17:48:07 +0300

    feat: add read-only methods to a QueryTracer

--- mongoengine_migrate/query_tracer.py
+++ mongoengine_migrate/query_tracer.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from typing import NamedTuple, Dict, Tuple, Any
 
 import wrapt
-import functools
 from bson import ObjectId
+
+_sentinel = object()
 
 
 class HistoryCall(NamedTuple):
@@ -53,20 +54,15 @@ class BulkWriteResultMock(NamedTuple):
     upserted_ids: Tuple[ObjectId] = (ObjectId('000000000000000000000000'), )
 
 
-def history_method(f):
-    @functools.wraps(f)
+def make_history_method(func_name, return_value=_sentinel):
     def w(instance, *args, **kwargs):
-        instance.call_history.append(
-            HistoryCall(
-                collection_name=instance.__wrapped__.full_name,
-                method_name=f.__name__,
-                call_datetime=datetime.now(),
-                args=args,
-                kwargs=kwargs
-            )
-        )
-        return f(instance, *args, **kwargs)
+        instance.add_history_call(func_name, args, kwargs)
 
+        if return_value == _sentinel:
+            f = getattr(instance.__wrapped__, func_name)
+            return f(*args, **kwargs)
+
+        return return_value
     return w
 
 
@@ -80,104 +76,58 @@ class QueryTracer(wrapt.ObjectProxy):
         super().__init__(*args, **kwrags)
         self.call_history = []
 
-    @history_method
-    def bulk_write(self, *args, **kwargs):
-        return BulkWriteResultMock()
+    # Collection modification methods
+    bulk_write = make_history_method('bulk_write', return_value=BulkWriteResultMock())
+    insert_one = make_history_method('insert_one', return_value=InsertOneResultMock())
+    insert_many = make_history_method('insert_many', return_value=InsertManyResultMock())
+    replace_one = make_history_method('replace_one', return_value=UpdateResultMock())
+    update_one = make_history_method('update_one', return_value=UpdateResultMock())
+    update_many = make_history_method('update_many', return_value=UpdateResultMock())
+    drop = make_history_method('drop', return_value=None)
+    delete_one = make_history_method('delete_one', return_value=DeleteResultMock())
+    delete_many = make_history_method('delete_many', return_value=DeleteResultMock())
+    create_indexes = make_history_method('create_indexes', return_value=[])
+    create_index = make_history_method('create_index', return_value=None)
+    ensure_index = make_history_method('ensure_index', return_value=None)
+    drop_indexes = make_history_method('drop_indexes', return_value=None)
+    drop_index = make_history_method('drop_index', return_value=None)
+    reindex = make_history_method('reindex', return_value=None)
+    rename = make_history_method('rename', return_value=None)
+    find_one_and_delete = make_history_method('find_one_and_delete', return_value=None)
+    find_one_and_replace = make_history_method('find_one_and_replace', return_value=None)
+    find_one_and_update = make_history_method('find_one_and_update', return_value=None)
+    save = make_history_method('save', return_value=ObjectId('000000000000000000000000'))
+    insert = make_history_method('insert', return_value=[])
+    update = make_history_method('update', return_value=[])
+    remove = make_history_method('remove', return_value=None)
+    find_and_modify = make_history_method('find_and_modify', return_value=None)
 
-    @history_method
-    def insert_one(self, *args, **kwargs):
-        return InsertOneResultMock()
+    # Aggregation methods
+    aggregate = make_history_method('aggregate', return_value=[])
+    aggregate_raw_batches = make_history_method('aggregate_raw_batches', return_value=[])
 
-    @history_method
-    def insert_many(self, *args, **kwargs):
-        return InsertManyResultMock()
+    # Collection reading methods
+    find_one = make_history_method('find_one')
+    find = make_history_method('find')
+    find_raw_batches = make_history_method('find_raw_batches')
+    estimated_document_count = make_history_method('estimated_document_count')
+    count_documents = make_history_method('count_documents')
+    list_indexes = make_history_method('list_indexes')
+    index_information = make_history_method('index_information')
+    options = make_history_method('options')
+    watch = make_history_method('watch')
+    group = make_history_method('group')
+    distinct = make_history_method('distinct')
+    map_reduce = make_history_method('map_reduce')
+    inline_map_reduce = make_history_method('inline_map_reduce')
 
-    @history_method
-    def replace_once(self, *args, **kwargs):
-        return UpdateResultMock()
-
-    @history_method
-    def update_one(self, *args, **kwargs):
-        return UpdateResultMock()
-
-    @history_method
-    def update_many(self, *args, **kwargs):
-        return UpdateResultMock()
-
-    @history_method
-    def drop(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def delete_one(self, *args, **kwargs):
-        return DeleteResultMock()
-
-    @history_method
-    def delete_many(self, *args, **kwargs):
-        return DeleteResultMock()
-
-    @history_method
-    def create_indexes(self, *args, **kwargs):
-        return []
-
-    @history_method
-    def create_index(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def ensure_index(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def drop_indexes(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def drop_index(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def reindex(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def aggregate(self, *args, **kwargs):
-        return []
-
-    @history_method
-    def rename(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def find_one_and_delete(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def find_one_and_replace(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def find_one_and_update(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def save(self, *args, **kwargs):
-        return ObjectId('000000000000000000000000')
-
-    @history_method
-    def insert(self, *args, **kwargs):
-        return []
-
-    @history_method
-    def update(self, *args, **kwargs):
-        return []
-
-    @history_method
-    def remove(self, *args, **kwargs):
-        pass
-
-    @history_method
-    def find_and_modify(self, *args, **kwargs):
-        pass
-
-    # TODO: implement read methods with appropriate mark in history_method
+    def add_history_call(self, func_name, args, kwargs):
+        self.call_history.append(
+            HistoryCall(
+                collection_name=self.__wrapped__.full_name,
+                method_name=func_name,
+                call_datetime=datetime.now(),
+                args=args,
+                kwargs=kwargs
+            )
+        )

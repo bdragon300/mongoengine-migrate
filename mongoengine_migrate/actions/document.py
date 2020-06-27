9666f526604b9599f5084a67426d2c9074cb8a8b
Igor Derkach <gosha753951@gmail.com>
2020-06-27 18:40:09 +0300

    feat: implement AlterDocument action

--- mongoengine_migrate/actions/document.py
+++ mongoengine_migrate/actions/document.py
@@ -1,6 +1,8 @@
+from copy import deepcopy
+
 from mongoengine_migrate.flags import EMBEDDED_DOCUMENT_NAME_PREFIX
 from mongoengine_migrate.schema import Schema
-from .base import BaseCreateDocument, BaseDropDocument, BaseRenameDocument
+from .base import BaseCreateDocument, BaseDropDocument, BaseRenameDocument, BaseDocumentAction
 
 
 class CreateDocument(BaseCreateDocument):
@@ -17,7 +19,7 @@ class CreateDocument(BaseCreateDocument):
 
         if document_type not in left_schema and document_type in right_schema:
             return cls(document_type=document_type,
-                       collection=right_schema[document_type].parameters['collection'])
+                       collection=right_schema[document_type].parameters.get('collection'))
 
     def to_schema_patch(self, left_schema: Schema):
         item = Schema.Document()
@@ -76,11 +78,41 @@ class RenameDocument(BaseRenameDocument):
         return super(RenameDocument, cls).build_object(document_type, left_schema, right_schema)
 
     def run_forward(self):
-        collection_names = self._run_ctx['collection'].database.list_collection_names()
-        if self._run_ctx['collection'].name in collection_names:
-            self._run_ctx['collection'].rename(self.new_name)
+        """Rename document only in schema, so do nothing"""
 
     def run_backward(self):
+        """Rename document only in schema, so do nothing"""
+
+
+class AlterDocument(BaseDocumentAction):
+    @classmethod
+    def build_object(cls, document_type: str, left_schema: Schema, right_schema: Schema):
+        match = document_type in left_schema \
+                and document_type in right_schema \
+                and left_schema[document_type].parameters != right_schema[document_type].parameters
+        if match:
+            return cls(document_type=document_type, **right_schema[document_type].parameters)
+
+    def to_schema_patch(self, left_schema: Schema):
+        left_item = left_schema[self.document_type]
+        right_item = deepcopy(left_item)
+        right_item.properties.clear()
+        right_item.properties.update(self.parameters)
+
+        return [
+            ('remove', '', [(self.document_type, left_item)]),
+            ('add', '', [(self.document_type, right_item)])
+        ]
+
+    def run_forward(self):
+        # Rename collection
         collection_names = self._run_ctx['collection'].database.list_collection_names()
         if self._run_ctx['collection'].name in collection_names:
-            self._run_ctx['collection'].rename(self.document_type)
+            self._run_ctx['collection'].rename(self.parameters['collection'])
+
+    def run_backward(self):
+        # Rename collection
+        collection_names = self._run_ctx['collection'].database.list_collection_names()
+        if self._run_ctx['collection'].name in collection_names:
+            new_name = self._run_ctx['left_schema'][self.document_type].parameters['collection']
+            self._run_ctx['collection'].rename(new_name)

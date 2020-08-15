19b24b858b0ef0eb5a1affd128e5f106b72503ab
Igor Derkach <gosha753951@gmail.com>
2020-08-15 13:34:45 +0300

    fix: use by_doc callback in dynamic change method in AlterEmbedded action

--- mongoengine_migrate/actions/embedded.py
+++ mongoengine_migrate/actions/embedded.py
@@ -9,10 +9,9 @@ import logging
 
 from mongoengine_migrate.flags import EMBEDDED_DOCUMENT_NAME_PREFIX
 from mongoengine_migrate.schema import Schema
-from mongoengine_migrate.utils import Diff, UNSET, document_type_to_class_name
-from mongoengine_migrate.mongo import mongo_version
-from ..updater import ByPathContext, DocumentUpdater
+from mongoengine_migrate.utils import Diff, document_type_to_class_name
 from .base import BaseCreateDocument, BaseDropDocument, BaseRenameDocument, BaseAlterDocument
+from ..updater import ByDocContext, DocumentUpdater
 
 log = logging.getLogger('mongoengine-migrate')
 
@@ -105,28 +104,26 @@ class AlterEmbedded(BaseAlterDocument):
         # TODO: raise error if other documents use the same collection
         #       when inherit becoming False
 
-    @mongo_version(min_version='2.6')
     def change_dynamic(self, diff: Diff):
-        return  # FIXME: fix all below
-        def by_path(ctx: ByPathContext):
-            dotpaths = {f'{ctx.filter_dotpath}.{k}': 1 for k in self_schema.keys()}
-            ctx.collection.aggregate([
-                {'$match': ctx.extra_filter},
-                {'$project': dotpaths},
-                {'$out': ctx.collection.name}  # >= 2.6
-            ])  # FIXME: consider _cls for inherited documents
+        """If document becomes non-dynamic then remove fields which
+        are not defined in mongoengine Document
+        """
+        def by_doc(ctx: ByDocContext):
+            extra_keys = ctx.document.keys() - self_schema.keys()
+            if extra_keys:
+                newdoc = {k: v for k, v in ctx.document.items() if k in self_schema.keys()}
+                ctx.document.clear()
+                ctx.document.update(newdoc)
 
         self._check_diff(diff, False, bool)
         if diff.new is True:
             return  # Nothing to do
 
         # Remove fields which are not in schema
-        self_schema = self._run_ctx['left_schema'][self.document_type]
+        self_schema = self._run_ctx['left_schema'][self.document_type]  # type: Schema.Document
         inherit = self_schema.parameters.get('inherit')
         document_cls = document_type_to_class_name(self.document_type) if inherit else None
-        updater = DocumentUpdater(self._run_ctx['db'],
-                                  self.document_type,
-                                  self._run_ctx['left_schema'],
-                                  None,
-                                  document_cls)
-        updater.update_by_path(by_path)
+        updater = DocumentUpdater(self._run_ctx['db'], self.document_type,
+                                  self._run_ctx['left_schema'], '',
+                                  self._run_ctx['migration_policy'], document_cls)
+        updater.update_by_document(by_doc)
